bdcd9e717e615879c67f6b4a5050627c6c3a604f
korobkad3v <korobka.d3v@gmail.com>
2025-02-19 21:51:37 -0800

    deleted unnecessary packages

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -6,15 +6,7 @@ public class Projectile : MonoBehaviour
 {
    private void OnCollisionEnter(Collision collision)
     {
-        
-        if (collision.gameObject.tag == "Asteroid")
-        {
-            var fracture = collision.gameObject.GetComponent<Fracture>();
-            if (fracture != null)
-            {
-                fracture.FractureObject();
-            }
-        }
+    
 
         if (collision.gameObject.tag != "Bullet")
         {
